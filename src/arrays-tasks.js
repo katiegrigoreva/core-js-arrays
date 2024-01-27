5926098d5183ee2a7e0b9c7a66ea4ed2ecedf69a
Kate <e.khizova@gmail.com>
2024-01-28 00:26:17 +0300

    feat: add removeFalsyValues, getStringsLength, getAverage solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -107,8 +107,8 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((el) => Boolean(el) === true);
 }
 
 /**
@@ -121,8 +121,8 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((el) => el.length);
 }
 
 /**
@@ -139,8 +139,18 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = arr.reduce(function (prevSum, cur) {
+    return prevSum + cur;
+  }, 0);
+  const res = sum / arr.length;
+  if (sum === 0) {
+    return sum;
+  }
+  if (Number.isInteger(res)) {
+    return res;
+  }
+  return +res.toFixed(2);
 }
 
 /**
