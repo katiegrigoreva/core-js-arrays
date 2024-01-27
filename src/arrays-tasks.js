eb4ab5d65ffb1fc36eaca345e76b725ce1d32746
Kate <e.khizova@gmail.com>
2024-01-27 23:26:12 +0300

    feat: add findElement and findAllOccurrences solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -63,8 +63,11 @@ function sumArrays(arr1, arr2) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  if (arr.includes(value)) {
+    return arr.indexOf(value);
+  }
+  return -1;
 }
 
 /**
@@ -81,8 +84,15 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let res = 0;
+  function count(el) {
+    if (el === item) {
+      res += 1;
+    }
+  }
+  arr.map((el) => count(el));
+  return res;
 }
 
 /**
